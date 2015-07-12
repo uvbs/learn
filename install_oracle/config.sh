15b15603a6d0402a37a148a382d79fdee3f7f513
sudoku.huang <sudoku@ubuntu.(none)>
2015-07-12 10:06:03 +0800

    1. add script to install oracle.

---
+++ install_oracle/config.sh
@@ -0,0 +1,13 @@
+# !/bin/sh
+
+sudo ln -s /usr/bin/awk /bin/awk
+sudo ln -s /usr/bin/rpm /bin/rpm
+sudo ln -s /usr/bin/basename /bin/basename
+sudo mkdir /etc/rc.d
+sudo ln -s /etc/rc0.d /etc/rc.d/rc0.d
+sudo ln -s /etc/rc2.d /etc/rc.d/rc2.d
+sudo ln -s /etc/rc3.d /etc/rc.d/rc3.d
+sudo ln -s /etc/rc4.d /etc/rc.d/rc4.d
+sudo ln -s /etc/rc5.d /etc/rc.d/rc5.d
+sudo ln -s /etc/rc6.d /etc/rc.d/rc6.d
+sudo ln -s /etc/init.d /etc/rc.d/init.d
